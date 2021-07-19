e40a632180c004214683f1a3776d845053a72a37
RA3-SIOUD <sioudlmo@gmail.com>
2021-07-19 10:40:10 +0100

    add greeting method

--- Main.java
+++ Main.java
@@ -2,6 +2,12 @@
 public class Main{
        public static void main(String[] argv){	
 	       System.out.println("Hello World");
+                greeting("Omrane");
        }
+
+       public static void greeting(String name){
+	       System.out.println("Hello, "+name);
+       }
+
 }
 
