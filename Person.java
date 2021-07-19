79d3dfc8975353971c97230a75829252539c3327
RA3-SIOUD <sioudlmo@gmail.com>
2021-07-19 10:52:16 +0100

    add Person Class

---
+++ Person.java
@@ -0,0 +1,14 @@
+public class Person{
+    private String name;
+    private String gender;
+    public Person(String name,String gender){
+
+    }
+
+    public String getName(){
+        return this.name;
+    }
+    public void setName(String value){
+        this.name = value;
+    }
+}
